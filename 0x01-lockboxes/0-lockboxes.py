38250dfc1a08a646b5aa9d300b378844cab2f280
endywezy <endywezy@gmail.com>
2024-08-01 14:21:16 +0100

    Refactor canUnlockAll function for improved readability and performance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,24 +7,18 @@ This module contains a function to determine if all boxes can be unlocked.
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-
-    Args:
-    boxes (list): A list of lists representing the keys in each box.
-
-    Returns:
-    bool: True if all boxes can be opened, False otherwise.
     """
     if not boxes:
         return False
 
     unlocked = [False] * len(boxes)
-    unlocked[0] = True  # The first box is unlocked
-    keys_to_try = boxes[0]  # Start with the keys in the first box
+    unlocked[0] = True
+    keys_to_try = boxes[0]
 
     while keys_to_try:
-        key = keys_to_try.pop(0)  # Get the next key
+        key = keys_to_try.pop(0)
         if key < len(boxes) and not unlocked[key]:
-            unlocked[key] = True  # Unlock the box
+            unlocked[key] = True
             keys_to_try.extend(boxes[key])
 
-    return all(unlocked)  # Return True, False otherwise
+    return all(unlocked)
