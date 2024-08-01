5b7c5746788eb1d0628e35078f722f9ef3ed27e6
endywezy <endywezy@gmail.com>
2024-08-01 13:50:06 +0100

    chore: Refactor canUnlockAll function for improved readability and performance

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 
-
 def canUnlockAll(boxes):
-    # Initialize the sets
-    keys = set()
-    opened_boxes = set()
+    """
+    Determines if all boxes can be opened.
 
-    keys.add(0)
-    opened_boxes.add(0)
+    Args:
+    boxes (list): A list of lists representing the keys in each box.
 
-    while keys:
-        current_key = keys.pop()
-        if current_key in opened_boxes:
-            continue
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes:  # If the input list is empty
+        return False  # Return False
 
-        opened_boxes.add(current_key)
+    unlocked = [False] * len(boxes)  # Initialize a list to track unlocked boxes
+    unlocked[0] = True  # The first box is unlocked
+    keys_to_try = boxes[0]  # Start with the keys in the first box
 
-        for key in boxes[current_key]:
-            if key not in opened_boxes:
-                keys.add(key)
+    while keys_to_try:  # Continue until there are no more keys to try
+        key = keys_to_try.pop(0)  # Get the next key
+        if key < len(boxes) and not unlocked[key]:  # If the key corresponds to a box and it's not unlocked
+            unlocked[key] = True  # Unlock the box
+            keys_to_try.extend(boxes[key])  # Add the keys from the newly unlocked box to the list of keys to try
 
-    return len(opened_boxes) == len(boxes)
+    return all(unlocked)  # Return True if all boxes are unlocked, False otherwise
