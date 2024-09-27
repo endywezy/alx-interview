309c2fbab6369cc71b16ff14cbe810c57a1d1a5d
endywezy <endywezy@gmail.com>
2024-09-27 01:34:58 +0100

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,16 +9,11 @@ def island_perimeter(grid):
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-                # Add 4 for each land cell
                 perimeter += 4
 
-                # Check for neighboring land
-                # cells and subtract for shared borders
                 if r > 0 and grid[r - 1][c] == 1:
                     perimeter -= 2
-                    # Subtract 2 for the shared border with the cell above
                 if c > 0 and grid[r][c - 1] == 1:
                     perimeter -= 2
-                # Subtract 2 for the shared border with the cell to the left
 
     return perimeter
